8ad7889ca9e89277a9d1d7360f3d23f2f2e2f2fb
mehmehmehlol <hkmeganlo@gmail.com>
2020-10-09 14:18:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// };
+
+// let configurationObject = {
+//     method = "POST",
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//     },
+//     body: JSON.stringify(formData)
+// };
+
+// fetch("http: //localhost:3000/dogs", configurationObject)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnorok!");
+//         console.log(error.message);
+//     });
+
+
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                email
+            })
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object['id']
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message
+        })
+
+}
